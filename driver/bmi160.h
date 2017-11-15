5baf4f1c9b7f5e6e831c40be17ec09af68f8d36f
anthony.shan <anthony.shan@goertek.com>
2017-11-15 10:42:04 +0800

    1: add driver and src folders 2: add bmi160.h and bmi160.c files

---
+++ driver/bmi160.h
@@ -0,0 +1,10 @@
+#ifndef				_BMI160_H_
+#defien				_BMI160_H_
+
+#define				ACC_RAW_REG				0x100
+
+
+
+void Sensor_IO_Read(void);
+
+#endif
---
+++ src/bmi160.c
@@ -0,0 +1,6 @@
+#include			"bmi160.h"
+
+void Sensor_IO_Read(void)
+{
+	
+}
